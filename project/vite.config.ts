b2e3e3e5e1226f75084b3eb175bdf2394bd9dbaf
kottanaindrakiran <kottanaindrakiran@gmail.com>
2025-05-11 23:28:53 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,23 +10,15 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['agora-rtc-sdk-ng'],
-    },
-    // Add base path for correct asset loading on Vercel
-    base: './',
-  },
+  base: "/", // Use "/" for Vercel or Netlify (don't use "./")
   define: {
-    'process.env': {},
+    "process.env": {},
   },
 }));
-
